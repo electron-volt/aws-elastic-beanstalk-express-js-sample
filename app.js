5450335655a7191428f99132fca1063ddb20541f
Eve <eve@Eves-MacBook-Air.local>
2022-01-13 16:25:46 +0200

    changes to app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hey you guys!!'));
+app.get('/', (req, res) => res.send('This text will be shown in Elastic Beanstalk!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
